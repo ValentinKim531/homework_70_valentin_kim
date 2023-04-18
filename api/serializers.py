e9754403cbff0ee8cc9b8b2df33801646d002f6f
Valentin Kim <valentink@yandex.ru>
2023-04-18 16:45:07 +0600

    Обновлен README.md
    руководство пользователя для API тасктрекера

--- api/serializers.py
+++ api/serializers.py
@@ -3,16 +3,21 @@ from webapp.models import Issue, Project
 
 
 class IssueSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Issue
-        fields = ('id', 'summary', 'description', 'type', 'status', 'project')
-        read_only_fields = ('id',)
+        fields = ("id", "summary", "description", "type", "status", "project")
+        read_only_fields = ("id",)
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Project
-        fields = ('id', 'start_date', 'end_date', 'title', 'description', 'users')
-        read_only_fields = ('id',)
+        fields = (
+            "id",
+            "start_date",
+            "end_date",
+            "title",
+            "description",
+            "users",
+        )
+        read_only_fields = ("id",)
